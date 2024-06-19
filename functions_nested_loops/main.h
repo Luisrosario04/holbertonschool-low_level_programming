e2d762c9728b44187109387fffacf0353d19d3e4
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-06-19 11:51:45 -0400

    0

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _puchart(char C);
+int _putchar(char C);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -8,4 +8,5 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 void print_to_98(int n);
