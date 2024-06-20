00ad17541305e1b705735f11c2a4caf617068d93
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-06-20 15:03:49 -0400

    o

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
-*_isupper - check for uppercase charecter
- *@ c: The character to be checked
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
  *
- *  Return: 1 if c is uppercase, otherwise 0
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 
 {
-
 	if (c >= 'A' && c <= 'Z')
-
-	return (1);
+		return (1);
 
 	else
-	return (0);
+		return (0);
 }
