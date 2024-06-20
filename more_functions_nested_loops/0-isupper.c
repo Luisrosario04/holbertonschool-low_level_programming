0d0ef282b2c2ce78c1bc4c10e36da5726efae7bd
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-06-20 13:34:16 -0400

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- isupper - check the code
- *
+*_isupper - check for uppercase charecter
+ *@ interger
  *  Return: Always (0)
  */
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
 
