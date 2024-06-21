9f794ff272e7ab0f9b80f72dac1120dc6053cf8f
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-06-20 20:50:36 -0400

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _isdigital - check for digit (0-9)
+ *  @: The number to be check.
+ *
+ *  Return: 1 if the number is a digit , otherwise 0.
+ */
+int _isdigital(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
+}
