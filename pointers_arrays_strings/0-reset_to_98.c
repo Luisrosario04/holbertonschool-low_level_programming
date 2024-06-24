5a005996efa0de11b4deeffa8989c391d19be7e1
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-06-24 12:35:52 -0400

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _reset to 98- Update de value it points to
- *@pointer
+ *@n: pointer
  * Return: Always (0)
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
