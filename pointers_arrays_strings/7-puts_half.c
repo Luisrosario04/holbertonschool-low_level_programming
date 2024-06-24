58e18f559b953407f23eb72c08dc7e5c791cbbb7
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-06-24 19:45:47 -0400

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,33 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * puts_half - prints half of a string
- * followed by a new line
- * @str: string to be printed
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
  */
-void puts_half(char *str)
+int main(void)
 {
-	int len, n, i;
+	int i;
 
-	len = 0;
-
-	while (str[len] != '\0')
+	for (i = 122; i > 96; i--)
 	{
-		len++;
+		putchar(i);
 	}
-
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	} else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; n++)
-		{
-			_putchar(str[n + 1]);
-		}
-	}
-	_putchar('\n');
+	putchar('\n');
+	return (0);
 }
