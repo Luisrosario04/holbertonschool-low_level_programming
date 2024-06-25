afca87f1eaf7cf977e558a259908385086c94d11
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-06-25 14:38:19 -0400

    5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,28 +1,21 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
- * @s: string to be reversed
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
  */
-void rev_string(char *s)
+char *string_toupper(char *s)
 {
-	char tmp;
-	int i, len, len1;
 
-	len = 0;
-	len1 = 0;
+	int i;
 
-	while (s[len] != '\0')
+i = 0;
+	while (*(s + i))
 	{
-		len++;
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
 	}
-
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
-	}
+	return (s);
 }
