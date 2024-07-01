13d99908fb439f0132ac35ae1cc9dbc78c2e9640
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-07-01 11:50:26 -0400

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts_recursion-print a string
+ *@s:string
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 1)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	putchar('\n');
+}
