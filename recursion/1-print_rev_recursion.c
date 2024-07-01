097df7f3496e4ec848054045f0104b3b630457b0
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-07-01 14:21:48 -0400

    1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *_print_rev_recursion - print strings in reverse.
- *@s: char string
- *Return: Always 0.
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-		_putchar (*s);
 }
