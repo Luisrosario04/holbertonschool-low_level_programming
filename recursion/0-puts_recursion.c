2ac51497812b806af84d5c6b2093fb7a02a75082
L-rosario <9481@holbertonstudents.comgit config --global user.email 9481@holbertonstudents.com>
2024-07-01 11:55:12 -0400

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- *_puts_recursion-print a string
- *@s:string
- * Return: void.
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 1)
+	if (*s == '\0')
 	{
-		putchar(*s);
-		_puts_recursion(s + 1);
+		putchar('\n');
+		return;
 	}
-	putchar('\n');
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
